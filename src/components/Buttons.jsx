b5588303375cb329282df51db254e69ea11eacd9
dewaraija <raijadewa@gmail.com>
2020-12-16 13:22:40 +0800

    Update Buttons.jsx

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -17,7 +17,7 @@ export default class Buttons extends Component {
           </button>
           {(this.props.connected || this.props.disconnected) && (
             <button className="next" onClick={this.props.next}>
-              Next <Next />
+              Skip <Next />
             </button>
           )}
         </div>
