fae6f3003df90f955e55726b765a524656b6fd45
Syeikh Anugrah <syeikhanugrah@gmail.com>
2021-01-15 21:13:56 +0700

    Polishing control button

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -23,6 +23,21 @@ export default class Buttons extends Component {
                   Skip
                 </button>
               )}
+              <button onClick={signOut} style={{
+                margin: "0 10px",
+                padding: "15px",
+                outline: "none",
+                border: "none",
+                borderRadius: "10px",
+                transition: "0.2s",
+                cursor: "pointer",
+                height: "70px",
+                width: "100px",
+                fontSize: "100%",
+                alignItems: "center"
+              }}>
+                Sign out
+              </button>
             </>
           ) : ''}
         </div>
--- src/components/SocialMediaButton.jsx
+++ src/components/SocialMediaButton.jsx
@@ -5,35 +5,12 @@ export default function SocialMediaButton({ user, signOut, signInWithFacebook, s
   return (
     <>
       {
-        user
-          ? <p style={{ color: '#fff' }}>Hello, { user.displayName }</p>
-          : ''
-      }
-
-      {
-        user
-          ?
-            <button onClick={signOut} style={{
-              margin: "0 10px",
-              padding: "15px",
-              outline: "none",
-              border: "none",
-              borderRadius: "10px",
-              transition: "0.2s",
-              cursor: "pointer",
-              height: "70px",
-              width: "100px",
-              fontSize: "100%",
-              alignItems: "center"
-            }}>
-              Sign out
-            </button>
-          : (
-            <>
-              <FacebookLoginButton onClick={signInWithFacebook} />
-              <GoogleLoginButton onClick={signInWithGoogle} />
-            </>
-          )
+        user === null && (
+          <>
+            <FacebookLoginButton onClick={signInWithFacebook} />
+            <GoogleLoginButton onClick={signInWithGoogle} />
+          </>
+        )
       }
     </>
   )
