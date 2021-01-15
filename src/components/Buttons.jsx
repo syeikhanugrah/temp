a8a8cfbc40f8ffb46ccee4113d56e3621a575c4a
Syeikh Anugrah <syeikhanugrah@gmail.com>
2021-01-15 13:56:13 +0700

    Check for user first

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -9,18 +9,22 @@ export default class Buttons extends Component {
     return (
       <div className="left">
         <div className="buttons">
-          <button
-            className="start"
-            style={{ backgroundColor: this.props.searching || this.props.connected ? '#fd4545' : '#51fd6d' }}
-            onClick={this.props.start}
-          >
-            {this.props.searching || this.props.connected ? 'Stop' : 'Start'}
-          </button>
-          {(this.props.connected || this.props.disconnected) && (
-            <button className="next" onClick={this.props.next}>
-              Skip
-            </button>
-          )}
+          {user ? (
+            <>
+              <button
+                className="start"
+                style={{ backgroundColor: this.props.searching || this.props.connected ? '#fd4545' : '#51fd6d' }}
+                onClick={this.props.start}
+              >
+                {this.props.searching || this.props.connected ? 'Stop' : 'Start'}
+              </button>
+              {(this.props.connected || this.props.disconnected) && (
+                <button className="next" onClick={this.props.next}>
+                  Skip
+                </button>
+              )}
+            </>
+          ) : ''}
         </div>
         <div>
           <SocialMediaButton user={user} signInWithFacebook={signInWithFacebook} signInWithGoogle={signInWithGoogle} signOut={signOut} />
